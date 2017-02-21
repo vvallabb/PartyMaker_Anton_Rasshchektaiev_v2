f78e53f38030154e7ea1b1fdb98d4b2b4b0708c3
anton1113 <anton11131113@gmail.com>
2017-02-21 14:50:18 +0200

    Images have been removed to assets

---
+++ Thing+CoreDataClass.m
@@ -0,0 +1,13 @@
+//
+//  Thing+CoreDataClass.m
+//  Party Maker
+//
+//  Created by intern on 2/15/17.
+//  Copyright © 2017 intern. All rights reserved.
+//
+
+#import "Thing.h"
+
+@implementation Thing
+
+@end
---
+++ Thing.h
@@ -0,0 +1,20 @@
+//
+//  Thing+CoreDataClass.h
+//  Party Maker
+//
+//  Created by intern on 2/15/17.
+//  Copyright © 2017 intern. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Thing : NSManagedObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "Thing+CoreDataProperties.h"
